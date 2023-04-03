9c53d388ec3862ea3769df6d53dd5ae217117f6a
vuivehieu <vuivehieu@gmail.com>
2023-04-03 14:54:28 +0700

    form check domain done

---
+++ src/main/java/com/example/asm/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.asm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String showForm(){
+        return "domain-form";
+    }
+    @PostMapping(value = "submit")
+    public String submitForm(){
+        return "redirect:/domain";
+    }
+}
