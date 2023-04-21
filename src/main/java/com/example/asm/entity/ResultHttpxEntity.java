1b38c01806de7296053ba10ccfa067803e131211
dangnh-dev <haidang14727@gmail.com>
2023-04-21 22:35:52 +0700

    cve và resulthttpx entity

---
+++ src/main/java/com/example/asm/entity/ResultHttpxEntity.java
@@ -0,0 +1,22 @@
+package com.example.asm.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_result_httpx")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResultHttpxEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "out_put")
+    private String outPut;
+    @ManyToOne(targetEntity = DomainEntity.class,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "domain_id")
+    private DomainEntity domainEntity;
+}
